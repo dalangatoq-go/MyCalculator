ce987955ba3fa68143f758374bff246d9c8a0389
dalangatoq-go <dalangatoq@gmail.com>
2026-07-05 03:26:24 +0700

    feat: isi firebaseConfig dari google-services.json (appId perlu dari Firebase Console)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,16 @@
-// placeholder
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD07QMa3KMvmjaABA4T7F4L-qmBeSj4Ugc",
+  authDomain: "mycalculator-pro123.firebaseapp.com",
+  projectId: "mycalculator-pro123",
+  storageBucket: "mycalculator-pro123.firebasestorage.app",
+  messagingSenderId: "791150769061",
+  appId: "REPLACE_WITH_WEB_APP_ID_FROM_FIREBASE_CONSOLE"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
