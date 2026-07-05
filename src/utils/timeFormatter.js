8da32e9b4e2b00ec875e0e8b38c13e7921d266e6
dalangatoq-go <dalangatoq@gmail.com>
2026-07-05 19:18:34 +0700

    fix: crash pada ChatRoomScreen setelah mount (useFirestore, ChatBubble, timeFormatter, FlatList)

--- src/utils/timeFormatter.js
+++ src/utils/timeFormatter.js
@@ -1,3 +1,10 @@
 export const formatTime = (date) => {
-  return new Date(date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  try {
+    if (date === null || date === undefined) return '';
+    const d = new Date(date);
+    if (isNaN(d.getTime())) return '';
+    return d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  } catch (e) {
+    return '';
+  }
 };
